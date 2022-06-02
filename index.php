dc181dc28b9a87b7d1c878f6473c8bcfaf1c1e7c
shimizu007 <106722081+shimizu007@users.noreply.github.com>
2022-06-02 14:16:48 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<?php
+$hands=['ぐー','ちょき','ぱー'];
+$picts=['gu','choki','pa'];
+$results=['あいこ','アナタのまけです...','アナタのかちです！'];
+if(isset($_POST['hand'])){
+  $userHand=(int)$_POST['hand'];
+  $pcHand=rand(0,count($hands)-1);
+}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/main.css">
+<title>じゃんけんぽん</title>
+</head>
+<body>
+<form method="post">
+<?php for($i=0;$i<count($hands);$i++):?>
+  <?php $checked=isset($userHand) && $userHand===$i ? 'checked':'';?>
+  <input type="radio" name="hand" value="<?=$i?>" <?=$checked?>><?=$hands[$i]?><br>
+<?php endfor;?>
+  <button type="submit">ショウブ</button>
+</form>
+<?php if(isset($_POST['hand'])):?>
+  <div>
+    <img src="images/<?=$picts[$userHand]?>.png">
+    <img src="images/<?=$picts[$pcHand]?>.png">
+  </div>
+  <p><?=$results[($userHand + 3 -$pcHand) % 3]?></p>
+<?php endif;?>
+</body>
+</html>
